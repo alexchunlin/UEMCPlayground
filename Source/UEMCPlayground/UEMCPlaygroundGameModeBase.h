4b09ee980231a8145c6c476cab24beb88210e313
alexchunlin <alexstephens38@gmail.com>
2023-07-25 22:16:31 -0400

    autogenerated blank project files

---
+++ Source/UEMCPlayground/UEMCPlaygroundGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UEMCPlaygroundGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UEMCPLAYGROUND_API AUEMCPlaygroundGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
